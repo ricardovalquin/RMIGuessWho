1edbee887e4d6a265f53ddf5d799f1f884fcc1cf
rick <ricardovalquin@gmail.com>
2015-04-15 15:51:44 -0500

    server finished

--- src/Interfaces/GuessWhoInterface.java
+++ src/Interfaces/GuessWhoInterface.java
@@ -6,34 +6,35 @@
 package Interfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  *
  * @author rick
  */
 public interface GuessWhoInterface extends Remote {
-    public boolean LogIn(String playerName);//
+    public boolean LogIn(String playerName) throws RemoteException;//
     
-    public boolean LOgOut(String playerName);//
+    public boolean LOgOut(String playerName)throws RemoteException;//
     
-    public String SeeOnlinePlayers();//
+    public String SeeOnlinePlayers()throws RemoteException;//
     
-    public boolean Challenge(String retador, String contrincante);//
+    public boolean Challenge(String retador, String contrincante)throws RemoteException;//
     
-    public int AnswerChallenges(String retado, String retador, String respuesta);
+    public int AnswerChallenges(String retado, String retador, String respuesta) throws RemoteException;
     
     // entrega un string que es el que te reta, VA EN EL HILO
-    public String AskByChallenges(String retado);
+    public String AskByChallenges(String retado) throws RemoteException;
     
-    public String SeeCharacter(int partida, String nombre);// see my character
+    public String SeeCharacter(int partida, String nombre) throws RemoteException;// see my character
     
-    public boolean AskCharacteristic(int partida, String player, String característica);
+    public boolean AskCharacteristic(int partida, String player, String característica) throws RemoteException;
     
-    public boolean AskCharacter (int partida, String player, String character);
+    public boolean AskCharacter (int partida, String player, String character) throws RemoteException;
     
-    public String SeeTurn(int partida);
+    public String SeeTurn(int partida) throws RemoteException;
     
-    public int SeeGame(String player);// retorna la partida que se está jugando
+    public int SeeGame(String player) throws RemoteException;// retorna la partida que se está jugando
     
 //    public String AskByGameState(int partida);
     
--- src/Server/GuesswhoServerTCP.java
+++ src/Server/GuesswhoServerTCP.java
@@ -8,44 +8,60 @@ package Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 
 /**
  *
  * @author rick
  */
 public class GuesswhoServerTCP {
-    private ServerSocket serverSocket = null;
-    private Socket GWClientSocket = null;
-    private GuessWhoServerThread GWThread = null;
-    private GuessWhoServerImplementation GWImplementation = null;
+//    private ServerSocket serverSocket = null;
+//    private Socket GWClientSocket = null;
+//    private GuessWhoServerThread GWThread = null;
+//    private GuessWhoServerImplementation GWImplementation = null;
     
-    public GuesswhoServerTCP(int port){
-        try{
-            GWImplementation = new GuessWhoServerImplementation();
-            serverSocket = new ServerSocket(port);
-        }catch(IOException e){
-            
-        }
-    }
+//    public GuesswhoServerTCP(int port){
+//        try{
+//            GWImplementation = new GuessWhoServerImplementation();
+//            serverSocket = new ServerSocket(port);
+//        }catch(IOException e){
+//            
+//        }
+//    }
     
-    public void daemon(){
-        while(true){
-            try{
-                System.out.println("Waiting for a player...");
-                GWClientSocket = serverSocket.accept();
-                System.out.println("Player founded");
-                System.out.println("asfd" + GWClientSocket.getInetAddress().getHostName());
-                 GWThread = new GuessWhoServerThread(GWClientSocket, GWImplementation);
-                 GWThread.start();
-            }catch(IOException e){
-                System.out.println("acá se totio");
-                e.printStackTrace();
-            }
-        }
-    }
+//    public void daemon(){
+//        while(true){
+//            try{
+//                System.out.println("Waiting for a player...");
+//                GWClientSocket = serverSocket.accept();
+//                System.out.println("Player founded");
+//                System.out.println("asfd" + GWClientSocket.getInetAddress().getHostName());
+//                 GWThread = new GuessWhoServerThread(GWClientSocket, GWImplementation);
+//                 GWThread.start();
+//            }catch(IOException e){
+//                System.out.println("acá se totio");
+//                e.printStackTrace();
+//            }
+//        }
+//    }
     
     public static void main(String arg[]){
-    GuesswhoServerTCP server = new GuesswhoServerTCP(2015);
-    server.daemon();
-}
+    //GuesswhoServerTCP server = new GuesswhoServerTCP(2015);
+    //server.daemon();
+    //GuessWhoServerImplementation serverImplementation = new GuessWhoServerImplementation();
+    //UnicastRemoteObject.exportObject(serverImplementation, 8888);
+        try {
+            LocateRegistry.createRegistry(1099);//rmiregistry
+            GuessWhoServerImplementation serverImplementation = new GuessWhoServerImplementation();
+            Naming.rebind("GuessWhoServer", serverImplementation);
+            System.out.println("Servidor del Guess Who Listo!");
+
+    }
+    catch (Exception e) {
+        System.out.println("Error in the server");
+    }
+    
+    }
 }
